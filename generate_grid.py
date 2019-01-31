b5e20676c59321464957dda7d0df8365d61e1040
Xiaoctw <m18846183092@163.com>
2019-02-01 07:52:38 +0800

    第三次提交

--- generate_grid.py
+++ generate_grid.py
@@ -12,23 +12,29 @@ def get_int(msg,minimum,default):
                 return i#如果无法正确输入，那么循环将一直进行下去，不会停止
         except ValueError as Err:
             print(Err)
-rows=get_int("rows:",1,None)
-columns=get_int("columns:",1,None)
-minimun=get_int("minimun(or Enter for 0):",-10000,0)
-default=1000
-if default<minimun:
-    default=2*minimun
-maximun=get_int("maximun(or Enter for"+str(default)+"):",minimun,default)
-row=0
-while row<rows:
-    line=""
-    column=0
-    while column<columns:
-        i=random.randint(minimun,maximun)
-        s=str(i)
-        while len(s)<10:
-            s=" "+s
-        line+=s
-        column+=1
-    print(line)
-    row+=1
+
+
+def main():
+    rows = get_int("rows:", 1, None)
+    columns = get_int("columns:", 1, None)
+    minimun = get_int("minimun(or Enter for 0):", -10000, 0)
+    default = 1000
+    if default < minimun:
+        default = 2 * minimun
+    maximun = get_int("maximun(or Enter for" + str(default) + "):", minimun, default)
+    row = 0
+    while row < rows:
+        line = ""
+        column = 0
+        while column < columns:
+            i = random.randint(minimun, maximun)
+            s = str(i)
+            while len(s) < 10:
+                s = " " + s
+            line += s
+            column += 1
+        print(line)
+
+
+if __name__ == "__main__":
+    main()
